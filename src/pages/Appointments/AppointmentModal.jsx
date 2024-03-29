818c93ff6789a27117fca35f7b015c0b7f03a1a4
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-03-29 18:48:43 +0100

    Update: styled button close modal

--- src/pages/Appointments/AppointmentModal.jsx
+++ src/pages/Appointments/AppointmentModal.jsx
@@ -76,6 +76,22 @@ const AppointmentModal = ({
         className="Modal"
         overlayClassName="Overlay"
       >
+        <CustomButton
+          className={"closeModalButton"}
+          title={
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-x-circle-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293z" />
+            </svg>
+          }
+          functionEmit={closeModal}
+        />
         <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
           {element.service.serviceName}
         </h2>
@@ -83,17 +99,19 @@ const AppointmentModal = ({
           <div>{element.service.description}</div>
           <div>{new Date(element.appointmentDate).toLocaleString()}</div>
         </div>
-        <CustomButton
-          className={"editAppointment"}
-          title={"edit"}
-          functionEmit={() => editAppointment(element.id)}
-        />
-        <CustomButton
-          className={"deleteAppointment"}
-          title={"delete"}
-          functionEmit={() => deleteMyAppointment(element.id)}
-        />
-        <button onClick={closeModal}>close</button>
+        <div className="buttons">
+          <CustomButton
+            className={"editAppointment"}
+            title={"edit"}
+            functionEmit={() => editAppointment(element.id)}
+          />
+          <CustomButton
+            className={"deleteAppointment"}
+            title={"delete"}
+            functionEmit={() => deleteMyAppointment(element.id)}
+          />
+          {/* <button onClick={closeModal}>close</button> */}
+        </div>
       </Modal>
     </div>
   )
