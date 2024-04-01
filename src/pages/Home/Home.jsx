65d6dc139d83b58457720f99fc7fcd0f6a8e8401
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-04-01 19:35:07 +0200

    Update: footer home, profile component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
         <Services />
         {tokenStorage && <Appointments />}
       </div>
-      <div className="footer">
+      <div className="footerHome">
         <div className="socialIcons">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -57,10 +57,10 @@ export const Home = () => {
             <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
           </svg>
         </div>
-        <div>Copyright 2024 Tattoo Shop Ltd. </div>
         <div>
           Trademarks and brands are the property of their respective owners.
         </div>
+        <div>Copyright 2024 Tattoo Shop Ltd. </div>
       </div>
     </>
   )
