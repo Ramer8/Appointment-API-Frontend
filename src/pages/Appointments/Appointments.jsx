e24d49208c62c96dc373f88831ccf0361ed1b677
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-04-01 13:00:25 +0200

    Update: styling appointment component

--- src/pages/Appointments/Appointments.jsx
+++ src/pages/Appointments/Appointments.jsx
@@ -136,7 +136,7 @@ const Appointments = () => {
         <div className="appointmentDesign">
           <div className="appointmentContainer">
             <div className="titleContainer">
-              <h2> Tattoo Appointment</h2>
+              <div> Tattoo Appointment</div>
               <CustomButton
                 className={"addAppointment"}
                 title={
@@ -155,112 +155,99 @@ const Appointments = () => {
               />
             </div>
             {toggleForm && (
-              <form method="post" onSubmit={handleSubmit}>
-                <label>Choose your service:</label>
-                <select
-                  name="id"
-                  defaultValue={services.id}
-                  className="selectAppointment"
+              <div className="appointmentNewBox">
+                <form
+                  method="post"
+                  onSubmit={handleSubmit}
+                  className="formAppointmentNew"
                 >
-                  {services.map((services) => (
-                    <option key={services.id} value={services.id}>
-                      {services.serviceName}
-                    </option>
-                  ))}
-                </select>
-                <button type="submit" className="addAppointment">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    className="bi bi-plus-circle-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
-                  </svg>
-                </button>
-                <div className="dateAndError">
-                  <CustomInput
-                    className={`inputDesign inputDate`}
-                    type={"datetime-local"}
-                    min={new Date()}
-                    placeholder={""}
-                    name={"appointment_date"}
-                    disabled={""}
-                    value={
-                      formatDate(
-                        newAppointment.appointment_date || new Date()
-                      ) || ""
-                    }
-                    functionChange={(e) => inputHandler(e)}
-                    // onBlurFunction={(e) => checkError(e)}
-                  />
-                  {msgError && <div className="errorDate">{msgError}</div>}
-                </div>
-              </form>
-            )}
-
-            <div className="appointmentList">
-              <div className="titleList">
-                <h2>Appointment list</h2>
-                {/* <CustomButton
-                  className={"addAppointment"}
-                  title={
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="22"
-                      fill="currentColor"
-                      className="bi bi-trash3-fill"
-                      viewBox="0 0 16 16"
+                  <label>Choose your service:</label>
+                  <div className="rowAppointmentNew">
+                    <select
+                      name="id"
+                      defaultValue={services.id}
+                      className="selectAppointmentList"
                     >
-                      <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
-                    </svg>
-                  }
-                  functionEmit={() => setToggleFormDelete(!toggleFormDelete)}
-                /> */}
-              </div>
-              {/* {toggleFormDelete && (
-                <form method="post" onSubmit={handleSubmit}>
-                  <label>Choose service to delete</label>
-                  <select
-                    name="id"
-                    defaultValue={services.id}
-                    className="selectAppointment"
-                  >
-                    {appointments.map((myAppointment) => (
-                      <option key={myAppointment.id} value={myAppointment.id}>
-                        {myAppointment.service.serviceName}
-                        {toggleModal && (
-                          <AppointmentModal
-                            myAppointment={myAppointment}
-                            tokenStorage={tokenStorage}
-                            setAppointmentChanged={setAppointmentChanged}
-                            appointmentChanged={appointmentChanged}
-                            services={services}
-                          />
-                        )}
-                      </option>
-                    ))}
-                  </select>
-                  <button type="submit">Submit</button>
+                      {services.map((services) => (
+                        <option key={services.id} value={services.id}>
+                          {services.serviceName}
+                        </option>
+                      ))}
+                    </select>
+                    <button type="submit" className="addAppointment">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="30"
+                        height="30"
+                        fill="currentColor"
+                        className="bi bi-plus-circle-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                      </svg>
+                    </button>
+                  </div>
+                  <div className="dateAndError">
+                    <CustomInput
+                      className={`inputDesign inputDate`}
+                      type={"datetime-local"}
+                      min={new Date()}
+                      placeholder={""}
+                      name={"appointment_date"}
+                      disabled={""}
+                      value={
+                        formatDate(
+                          newAppointment.appointment_date || new Date()
+                        ) || ""
+                      }
+                      functionChange={(e) => inputHandler(e)}
+                      // onBlurFunction={(e) => checkError(e)}
+                    />
+                    {msgError && <div className="errorDate">{msgError}</div>}
+                  </div>
                 </form>
-              )} */}
+              </div>
+            )}
+            <div className="appointmentList">
+              <div className="titleList">Appointment list</div>
               <hr />
               {!appointments.length && "Nothing Scheduled"}
               {appointments.map((myAppointment) => (
                 <div key={myAppointment.id} className="appointmentBox">
-                  <div>{myAppointment.service.serviceName}</div>
-                  <div>
-                    {new Date(myAppointment.appointmentDate).toLocaleString()}
+                  <div className="rowBox">
+                    <div className="leftSideBox">
+                      <div className="avatarService">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-flower1"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M6.174 1.184a2 2 0 0 1 3.652 0A2 2 0 0 1 12.99 3.01a2 2 0 0 1 1.826 3.164 2 2 0 0 1 0 3.652 2 2 0 0 1-1.826 3.164 2 2 0 0 1-3.164 1.826 2 2 0 0 1-3.652 0A2 2 0 0 1 3.01 12.99a2 2 0 0 1-1.826-3.164 2 2 0 0 1 0-3.652A2 2 0 0 1 3.01 3.01a2 2 0 0 1 3.164-1.826M8 1a1 1 0 0 0-.998 1.03l.01.091q.017.116.054.296c.049.241.122.542.213.887.182.688.428 1.513.676 2.314L8 5.762l.045-.144c.248-.8.494-1.626.676-2.314.091-.345.164-.646.213-.887a5 5 0 0 0 .064-.386L9 2a1 1 0 0 0-1-1M2 9l.03-.002.091-.01a5 5 0 0 0 .296-.054c.241-.049.542-.122.887-.213a61 61 0 0 0 2.314-.676L5.762 8l-.144-.045a61 61 0 0 0-2.314-.676 17 17 0 0 0-.887-.213 5 5 0 0 0-.386-.064L2 7a1 1 0 1 0 0 2m7 5-.002-.03a5 5 0 0 0-.064-.386 16 16 0 0 0-.213-.888 61 61 0 0 0-.676-2.314L8 10.238l-.045.144c-.248.8-.494 1.626-.676 2.314-.091.345-.164.646-.213.887a5 5 0 0 0-.064.386L7 14a1 1 0 1 0 2 0m-5.696-2.134.025-.017a5 5 0 0 0 .303-.248c.184-.164.408-.377.661-.629A61 61 0 0 0 5.96 9.23l.103-.111-.147.033a61 61 0 0 0-2.343.572c-.344.093-.64.18-.874.258a5 5 0 0 0-.367.138l-.027.014a1 1 0 1 0 1 1.732zM4.5 14.062a1 1 0 0 0 1.366-.366l.014-.027q.014-.03.036-.084a5 5 0 0 0 .102-.283c.078-.233.165-.53.258-.874a61 61 0 0 0 .572-2.343l.033-.147-.11.102a61 61 0 0 0-1.743 1.667 17 17 0 0 0-.629.66 5 5 0 0 0-.248.304l-.017.025a1 1 0 0 0 .366 1.366m9.196-8.196a1 1 0 0 0-1-1.732l-.025.017a5 5 0 0 0-.303.248 17 17 0 0 0-.661.629A61 61 0 0 0 10.04 6.77l-.102.111.147-.033a61 61 0 0 0 2.342-.572c.345-.093.642-.18.875-.258a5 5 0 0 0 .367-.138zM11.5 1.938a1 1 0 0 0-1.366.366l-.014.027q-.014.03-.036.084a5 5 0 0 0-.102.283c-.078.233-.165.53-.258.875a61 61 0 0 0-.572 2.342l-.033.147.11-.102a61 61 0 0 0 1.743-1.667c.252-.253.465-.477.629-.66a5 5 0 0 0 .248-.304l.017-.025a1 1 0 0 0-.366-1.366M14 9a1 1 0 0 0 0-2l-.03.002a5 5 0 0 0-.386.064c-.242.049-.543.122-.888.213-.688.182-1.513.428-2.314.676L10.238 8l.144.045c.8.248 1.626.494 2.314.676.345.091.646.164.887.213a5 5 0 0 0 .386.064zM1.938 4.5a1 1 0 0 0 .393 1.38l.084.035q.108.045.283.103c.233.078.53.165.874.258a61 61 0 0 0 2.343.572l.147.033-.103-.111a61 61 0 0 0-1.666-1.742 17 17 0 0 0-.66-.629 5 5 0 0 0-.304-.248l-.025-.017a1 1 0 0 0-1.366.366m2.196-1.196.017.025a5 5 0 0 0 .248.303c.164.184.377.408.629.661A61 61 0 0 0 6.77 5.96l.111.102-.033-.147a61 61 0 0 0-.572-2.342c-.093-.345-.18-.642-.258-.875a5 5 0 0 0-.138-.367l-.014-.027a1 1 0 1 0-1.732 1m9.928 8.196a1 1 0 0 0-.366-1.366l-.027-.014a5 5 0 0 0-.367-.138c-.233-.078-.53-.165-.875-.258a61 61 0 0 0-2.342-.572l-.147-.033.102.111a61 61 0 0 0 1.667 1.742c.253.252.477.465.66.629a5 5 0 0 0 .304.248l.025.017a1 1 0 0 0 1.366-.366m-3.928 2.196a1 1 0 0 0 1.732-1l-.017-.025a5 5 0 0 0-.248-.303 17 17 0 0 0-.629-.661A61 61 0 0 0 9.23 10.04l-.111-.102.033.147a61 61 0 0 0 .572 2.342c.093.345.18.642.258.875a5 5 0 0 0 .138.367zM8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
+                        </svg>
+                      </div>
+                      <div className="leftTextBox">
+                        <div className="serviceName">
+                          {myAppointment.service.serviceName}
+                        </div>
+                        <div className="date">
+                          {new Date(
+                            myAppointment.appointmentDate
+                          ).toDateString()}
+                        </div>
+                      </div>
+                    </div>
+                    <div className="rightSideBox">
+                      <AppointmentModal
+                        myAppointment={myAppointment}
+                        tokenStorage={tokenStorage}
+                        setAppointmentChanged={setAppointmentChanged}
+                        appointmentChanged={appointmentChanged}
+                      />
+                    </div>
                   </div>
-                  <AppointmentModal
-                    myAppointment={myAppointment}
-                    tokenStorage={tokenStorage}
-                    setAppointmentChanged={setAppointmentChanged}
-                    appointmentChanged={appointmentChanged}
-                  />
                 </div>
               ))}
             </div>
